6a9295f46d89837f4444a8e2a396e042255997b0
Chris Woodward <cwoodward5@hotmail.com>
2019-01-24 18:28:37 -0800

    Removed unused code

--- client/src/data/templates/footer.template.ts
+++
@@ -1,7 +0,0 @@
-export const FooterTemplate = {
-    raw: `
-        <div class="footer" id="header">
-            Hello I'm the header
-        </div>
-    `
-}  
--- client/src/data/templates/header.template.ts
+++
@@ -1,7 +0,0 @@
-export const HeaderTemplate = {
-    raw: `
-        <div class="header" id="header">
-            Hello I'm the header
-        </div>
-    `
-}    
